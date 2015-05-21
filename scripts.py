1477aa3eb1c0bd2d8d5365b3b09261ce0c09f915
Rodrigo Arias <rodarima@gmail.com>
2015-05-21 19:36:24 +0200

    Derrape funcional

--- scripts.py
+++ scripts.py
@@ -28,36 +28,74 @@ from bge import render
 
 import mathutils
 import math
+import time
+
+scale = 1
 
 ## Set specific vehicle characteristics ##
-wheel_radius = 0.65/2		# Radio de las ruedas
-wheel_wide = 1.0		# Separación entre ruedas (ancho)
-wheel_height = -0.2
-wheelFrontOffset = 1.25
-wheelBackOffset = -1.25
+wheel_radius = scale*0.65/2		# Radio de las ruedas
+wheel_wide = scale*1.0		# Separación entre ruedas (ancho)
+wheel_height = scale*0.2
+wheelFrontOffset = scale*1.25
+wheelBackOffset = scale*-1.25
 #AttachHeightLocal = 0.6		#0.2
-suspensionLength = 0.3		#0.8
+suspensionLength = scale*0.3		#0.8
 
 mass = 50.0
 
-influence = 0.05	#0.02
-stiffness = 50.0    #20.0	Dureza del amortiguador
-damping = 5	#2.0	Suavizado de la amortiguación
-compression = 10.0	#4.0	Resistencia a la compresión
-friction = 100.0		#10.0
+influence = 0.03	#0.02
+stiffness = 20.0    #20.0	Dureza del amortiguador
+damping = 5.0			#2.0	Suavizado de la amortiguación
+compression = 6.0	#4.0	Resistencia a la compresión
+friction = 6.0	#8.0
 Stability = 0.02	#0.05
-force = 400.0		#15.0
+force = 1500.0		#15.0
 ease_steer = 0.9	#0.6
 ease_force = 0.5	#0.9
 steer_inc = 0.03	#0.05
-rear_force = 0.2			# Porción de fuerza aplicada atrás
-brake_force = force*0.3
-brake_friction = friction * 0.5
+rear_force = 0.1			# Porción de fuerza aplicada atrás
+brake_force = 2
+brake_friction = friction * 0.4
 
 
 CHASSIS_NAME = "car_chassis"
 
+def car_friction(vehicle, front=None, back=None):
+	if front != None:
+		vehicle.setTyreFriction(front,0)
+		vehicle.setTyreFriction(front,1)
+	if back != None:
+		vehicle.setTyreFriction(back,2)
+		vehicle.setTyreFriction(back,3)
 
+def car_influence(vehicle, front=None, back=None):
+	if front != None:
+		vehicle.setRollInfluence(front, 0)
+		vehicle.setRollInfluence(front, 1)
+	if back != None:
+		vehicle.setRollInfluence(back, 2)
+		vehicle.setRollInfluence(back, 3)
+
+def car_brake(vehicle, front=None, back=None):
+	if front != None:
+		vehicle.applyBraking(front, 0)
+		vehicle.applyBraking(front, 1)
+	if back != None:
+		vehicle.applyBraking(back, 2)
+		vehicle.applyBraking(back, 3)
+
+def car_power(vehicle, front=None, back=None):
+	if front != None:
+		vehicle.applyEngineForce(front, 0)
+		vehicle.applyEngineForce(front, 1)
+	if back != None:
+		vehicle.applyEngineForce(back, 2)
+		vehicle.applyEngineForce(back, 3)
+	
+def car_steer(vehicle, steering):
+	vehicle.setSteeringValue(steering, 0)
+	vehicle.setSteeringValue(steering, 1)
+	
 
 def car_logic_init():
 	## setup aliases for Blender API access ##
@@ -86,6 +124,9 @@ def car_init():
 	logic.scene = logic.getCurrentScene()
 	#logic.car  = cont.owner
 
+	#logic.car["start_time"] = time.time()
+	#print("{} - {}".format(time.time(), logic.car["start_time"]))
+
 	## setup general vehicle characteristics ##
 	wheelAttachDirLocal = [0,0,-1]
 	wheelAxleLocal = [-1,0,0]
@@ -106,6 +147,7 @@ def car_init():
 	logic.car["steer_val"] = 0.0
 	logic.car["steer_s"] = 0.0
 	logic.car["km_h"] = 0.0
+	#logic.car["time"] = time.time() - logic.car["start_time"]
 	
 	logic.car["braking"] = False
 
@@ -120,7 +162,7 @@ def car_init():
 		wheel_name = wheels[i]
 		wheel = logic.scene.objects[wheel_name]
 		wheel_pos = wheel.worldPosition
-		wheel_local = wheel_pos - car_pos - mathutils.Vector((0.0, 0.0, wheel_height))
+		wheel_local = wheel_pos - car_pos - mathutils.Vector((0.0, 0.0, 0.0))#wheel_height - suspensionLength))
 
 		vehicle.addWheel(
 			wheel,
@@ -147,6 +189,7 @@ def draw_vec(pos, vec):
 ## called from main car object
 ## is run once at the start of the game
 def car_update():
+
 	#print("car update")
 	vehicle = constraints.getVehicleConstraint(logic.car["cid"])
 
@@ -160,6 +203,14 @@ def car_update():
 	draw_vec(pos, vel)
 	#draw_vec(pos, wheel_v)
 	#print(wheel_v)
+	
+
+	if "start_time" not in logic.car:
+		logic.car["start_time"] = time.time()*1000.0
+
+	logic.car["time"] = time.time()*1000.0 - logic.car["start_time"]
+	#print("{} - {}".format(time.time(), logic.car["start_time"]))
+	
 
 	#f = mathutils.Vector(logic.car.getReactionForce()) * 3
 	#print(f)
@@ -167,20 +218,34 @@ def car_update():
 	velm = math.sqrt(vel[0]**2 + vel[1]**2 + vel[2]**2)
 	#print(linSum)
 
-	## apply engine force ##
-	vehicle.applyEngineForce(logic.car["force"],0)
-	vehicle.applyEngineForce(logic.car["force"],1)
+
+
+	## brake back wheels
+	if logic.car["braking"]:
+		car_friction(vehicle, 8, 2)
+		car_influence(vehicle, 0.06, 0.01)
+		car_power(vehicle, logic.car["force"], 0)
+		car_brake(vehicle, 0, 2)
+	else:
+		car_friction(vehicle, 8, 10)
+		car_influence(vehicle, 0.06, 0.06)
+		car_power(vehicle, logic.car["force"], logic.car["force"] * rear_force)
+		car_brake(vehicle, 0, 0)
 
 	## calculate steering with varying sensitivity ##
 
 	speed = math.fabs(logic.car["speed"])
+	log_speed = math.log(speed * 0.06 + 1)
+	#s = 2 - log_speed
 	s = 2 - math.pow(speed * 0.06, 1/3)
-	if s < 0.07: s = 0.07
+	if s < 0.03: s = 0.03
 
-	steer_val = logic.car["steer"] * 1# * s
-	print(steer_val)
-	logic.car["steer_val"] = steer_val
-	logic.car["steer_s"] = s
+	steer_val = logic.car["steer"] * 1 * s
+
+	log_steer = max(1-math.log(speed+30)/5, 0.03)
+	#print(steer_val)
+	logic.car["steer_val"] = log_steer
+	logic.car["steer_s"] = log_steer
 	logic.car["km_h"] = velm
 
 #	if math.fabs(logic.car["speed"])<15.0: s = 2.0
@@ -190,25 +255,7 @@ def car_update():
 
 	## steer front wheels
 	#vehicle.setSteeringValue(logic.car["steer"] * s,0)
-	vehicle.setSteeringValue(steer_val, 0)
-	vehicle.setSteeringValue(steer_val, 1)
-
-	## brake back wheels
-	if logic.car["braking"]:
-		#vehicle.applyBraking(brake_force, 2)
-		#vehicle.applyBraking(brake_force, 3)
-		vehicle.setTyreFriction(brake_friction,2)
-		vehicle.setTyreFriction(brake_friction,3)
-		vehicle.applyEngineForce(-brake_force,2)
-		vehicle.applyEngineForce(-brake_force,3)
-		logic.car["braking"] = False
-		#print("braking {}".format(brake_force))
-	else:
-		vehicle.setTyreFriction(friction,2)
-		vehicle.setTyreFriction(friction,3)
-		vehicle.applyEngineForce(logic.car["force"] * rear_force,2)
-		vehicle.applyEngineForce(logic.car["force"] * rear_force,3)
-
+	car_steer(vehicle, steer_val)
 
 	## slowly ease off gas and center steering ##
 	logic.car["steer"] *= ease_steer
@@ -229,6 +276,7 @@ def key_update():
 	#print('key update')
 	cont = logic.getCurrentController()
 	keys = cont.sensors["key"].events
+	#logic.car["braking"] = False
 	#print(keys)
 	for key in keys:
 		## up arrow
@@ -255,18 +303,18 @@ def key_update():
 					logic.car.setAngularVelocity([0.0,0.0,0.0],1)
 					logic.car["jump"] = 0
 		## Spacebar
-		elif key[0] == events.SPACEKEY:
-			# hackish Brake
-			if logic.car["speed"] > 2.0:
-				logic.car["force"]  = force
-			if logic.car["speed"] < -2.0:
-				logic.car["force"]  = -force
-		elif key[0] in (events.LEFTCTRLKEY, events.RIGHTCTRLKEY):
-			logic.car["braking"] = True
-			#if key[1] == logic.KX_INPUT_JUST_ACTIVATED
-			#	logic.car["braking"] = True
-			#else:
-			#	logic.car["braking"] = False
+		#elif key[0] == events.SPACEKEY:
+		#	# hackish Brake
+		#	if logic.car["speed"] > 2.0:
+		#		logic.car["force"]  = force
+		#	if logic.car["speed"] < -2.0:
+		#		logic.car["force"]  = -force
+		elif key[0] == events.LEFTCTRLKEY:
+			#logic.car["braking"] = True
+			if key[1] == logic.KX_INPUT_JUST_ACTIVATED:
+				logic.car["braking"] = True
+			elif key[1] == logic.KX_INPUT_JUST_RELEASED:
+				logic.car["braking"] = False
 	
 #def on_collide():
 	#print("collide")
